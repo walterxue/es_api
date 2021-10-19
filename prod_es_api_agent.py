48382936f615a02e4ee9f727c04ac59f8c7da376
walterxue <walterxue@outlook.com>
2021-10-19 16:59:10 +0800

    add agent top10 code

--- prod_es_api_agent.py
+++ prod_es_api_agent.py
@@ -8,14 +8,14 @@ import argparse
 #es連線資訊
 def connection_info():
     es = Elasticsearch(["10.121.0.41","10.121.0.42","10.121.0.43","10.121.0.44","10.121.0.45","10.121.0.46"],
-        http_auth=('elastic', 'zT0YA5W01tcTlIi2De04')
+        http_auth=('watch_alert', '!QAZ2wsx')
     )
     return es
 
 def product_info(product):
     products = {
         'lc' : '10',
-        'v8' : '5',
+        'v8' : '3',
         'mp' : '2',
         'ly1' : '2',
         'ly2' : '10',
@@ -24,7 +24,7 @@ def product_info(product):
         'xsj2' : '2',
         'ky34' : '10',
         'ky110' : '10',
-        'ky508' : '10',
+        'ky508' : '1',
         'kydf' : '10',
         'kygpk' : '10',
         'kyt' : '2',
@@ -38,8 +38,8 @@ def telegram_bot_sendtext(bot_message):
     bot_token = '1854129202:AAE5amgijJpQVYPo2ARMZP4b7dvS7PlZuic'
     bot_chatID = '-512809025'
     #test channel 
-    #bot_token = '1882707925:AAFkhLbz45lZFrURcn_IQVsW8uNgfXrxbpo'
-    #bot_chatID = '999849909'
+#    bot_token = '1882707925:AAFkhLbz45lZFrURcn_IQVsW8uNgfXrxbpo'
+#    bot_chatID = '999849909'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
 
@@ -74,10 +74,10 @@ def check_agent_count(product):
             s = Search(using=es, index="logstash-"+product+"-channelhandle-out-*").query("match",agent=agent).filter('range',  ** {'@timestamp':{'gte': "now-3m" }})
             response =  s.execute()
             if response.hits.total.value <1 : 
-                bot_message = product + "代理："+str(agent) +"目前人數小於1，請確認代理是否維護中"
+                bot_message = product + " Agent："+str(agent) +" 目前人數小於0，請確認代理是否維護中!!"
                 telegram_bot_sendtext(bot_message)
 #            else:
-#                bot_message = product + "代理："+str(agent) +" 目前人數 "+ str(response.hits.total.value)
+#                bot_message = product + "Agent："+str(agent) +" 目前人數 "+ str(response.hits.total.value)
 #                telegram_bot_sendtext(bot_message)
     except :
         print(f"{product} 代理抓取失敗")
